84a03a0269b23ea46b65b289a1f5f2c3797a167d
Mariona Estrada <165200714+estradapm@users.noreply.github.com>
2024-06-18 10:46:49 +0000

    acabado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,45 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const palos = ["corazon", "pica", "trebol", "diamante"];
+  const valores = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  const simbolo = {
+    corazon: "♥",
+    pica: "♠",
+    trebol: "♣",
+    diamante: "♦"
+  };
+
+  const randomPalos = palos[Math.floor(Math.random() * palos.length)];
+  const randomValores = valores[Math.floor(Math.random() * valores.length)];
+
+  let cartaElemento = document.getElementById("card");
+  cartaElemento.className = `card ${randomPalos}`;
+
+  document.querySelector(
+    ".esquinaSuperior .numero"
+  ).textContent = randomValores;
+  document.querySelector(".esquinaSuperior .simbolo").textContent =
+    simbolo[randomPalos];
+  document.querySelector(".centro").textContent = simbolo[randomPalos];
+  document.querySelector(
+    ".esquinaInferior .numero"
+  ).textContent = randomValores;
+  document.querySelector(".esquinaInferior .simbolo").textContent =
+    simbolo[randomPalos];
 };
